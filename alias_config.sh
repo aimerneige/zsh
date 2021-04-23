de8d47ed8fa2a6b99744dede098b1807f2b1a100
AimerNeige <aimer.neige.soft@gmail.com>
2021-04-23 19:04:15 +0800

    first commit

---
+++ alias_config.sh
@@ -0,0 +1,49 @@
+#     _    _                     _   _      _            _     
+#    / \  (_)_ __ ___   ___ _ __| \ | | ___(_) __ _  ___( )___ 
+#   / _ \ | | '_ ` _ \ / _ \ '__|  \| |/ _ \ |/ _` |/ _ \// __|
+#  / ___ \| | | | | | |  __/ |  | |\  |  __/ | (_| |  __/ \__ \
+# /_/   \_\_|_| |_| |_|\___|_|  |_| \_|\___|_|\__, |\___| |___/
+#                                             |___/            
+#  _________  _   _ ____   ____      _    _ _           
+# |__  / ___|| | | |  _ \ / ___|    / \  | (_) __ _ ___ 
+#   / /\___ \| |_| | |_) | |       / _ \ | | |/ _` / __|
+#  / /_ ___) |  _  |  _ <| |___   / ___ \| | | (_| \__ \
+# /____|____/|_| |_|_| \_\\____| /_/   \_\_|_|\__,_|___/
+
+# Blog   https://aimerneige.com/
+# Email  aimer.neige@aimerneige.com
+# GitHub https://github.com/AimerNeige
+
+
+alias c="clear"
+alias cls="clear"
+alias sss="ssr start"
+alias sst="ssr stop"
+alias lg="lazygit"
+alias i="sudo apt install"
+alias iy="sudo apt install -y"
+alias gc="git clone"
+alias gcd="git clone --depth=1"
+alias sysinfo="neofetch"
+alias cs="cowsay"
+alias ud="sudo apt update"
+alias ug="sudo apt upgrade"
+alias udy="sudo apt update -y"
+alias dgy="sudo apt upgrade -y"
+alias udg="sudo apt update & sudo apt upgrade"
+alias udgy="sudo apt update -y & sudo apt upgrade -y"
+alias vim="nvim"
+alias zshload="source ~/.zshrc"
+alias zshedit="vim ~/.zshrc"
+alias zshconfig="mate ~/.zshrc"
+alias rg="ranger"
+alias pi="pip install"
+alias pi3="pip3 install"
+alias py="python"
+alias py3="python3"
+alias gits="git status"
+alias pipm="pip3 install --index-url https://pypi.douban.com/simple"
+# alias youtube-dl="youtube-dl --proxy \"socks5://127.0.0.1:1080\""
+# alias youtube-dlsuben="youtube-dl --proxy \"socks5://127.0.0.1:1080\" --write-srt --sub-lang en"
+# alias youtube-dlsubauto="youtube-dl --proxy \"socks5://127.0.0.1:1080\" --write-auto-sub"
+# alias pi3m="pip3 install -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com"
---
+++ path_config.sh
@@ -0,0 +1,33 @@
+#     _    _                     _   _      _            _     
+#    / \  (_)_ __ ___   ___ _ __| \ | | ___(_) __ _  ___( )___ 
+#   / _ \ | | '_ ` _ \ / _ \ '__|  \| |/ _ \ |/ _` |/ _ \// __|
+#  / ___ \| | | | | | |  __/ |  | |\  |  __/ | (_| |  __/ \__ \
+# /_/   \_\_|_| |_| |_|\___|_|  |_| \_|\___|_|\__, |\___| |___/
+#                                             |___/            
+#  _________  _   _ ____   ____   ____       _   _     
+# |__  / ___|| | | |  _ \ / ___| |  _ \ __ _| |_| |__  
+#   / /\___ \| |_| | |_) | |     | |_) / _` | __| '_ \ 
+#  / /_ ___) |  _  |  _ <| |___  |  __/ (_| | |_| | | |
+# /____|____/|_| |_|_| \_\\____| |_|   \__,_|\__|_| |_|
+
+# Blog   https://aimerneige.com/
+# Email  aimer.neige@aimerneige.com
+# GitHub https://github.com/AimerNeige
+
+
+# Homebrew
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+
+# Go
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/Code/golang
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+export PATH=$PATH:/usr/local/go/bin
+
+# pip
+export PATH=$PATH:$HOME/.local/bin
+
+# NDK
+export ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/22.1.7171670/
