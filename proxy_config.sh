fb6a09bd6d5411a8ecdf4041e636ebc86e0e1b41
AimerNeige <aimer.neige.soft@gmail.com>
2021-06-08 17:12:13 +0800

    update

--- proxy_config.sh
+++ proxy_config.sh
@@ -16,11 +16,11 @@
 # GitHub https://github.com/AimerNeige
 
 
-export https_proxy=socks5://127.0.0.1:1080
-export http_proxy=socks5://127.0.0.1:1080
+# export https_proxy=socks5://127.0.0.1:1080
+# export http_proxy=socks5://127.0.0.1:1080
 
-# export https_proxy=http://127.0.0.1:1080
-# export http_proxy=http://127.0.0.1:1080
+export https_proxy=http://127.0.0.1:1080
+export http_proxy=http://127.0.0.1:1080
 
 # export all_proxy=socks5://127.0.0.1:1080
 # export all_proxy=http://127.0.0.1:1080
