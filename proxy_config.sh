d2b73a1cab5c15c964933d4d13182d99395c0215
AimerNeige <aimer.neige.soft@gmail.com>
2021-06-14 02:24:21 +0800

    update proxu settings

--- proxy_config.sh
+++ proxy_config.sh
@@ -19,8 +19,8 @@
 # export https_proxy=socks5://127.0.0.1:1080
 # export http_proxy=socks5://127.0.0.1:1080
 
-export https_proxy=http://127.0.0.1:1080
-export http_proxy=http://127.0.0.1:1080
+# export https_proxy=http://127.0.0.1:1080
+# export http_proxy=http://127.0.0.1:1080
 
 # export all_proxy=socks5://127.0.0.1:1080
 # export all_proxy=http://127.0.0.1:1080
